5f862e4eb815bc71bafa412d40d1ff3a95bce813
derphilovic <philovic@outlook.de>
2024-12-12 21:11:42 +0100

    cart

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const cart = [
-{ id:1, name:"Ryzen_5_7500f", price:115, quantity:1,},    
-{ id:2, name:"Ryzen_5_7500f", price:115, quantity:1,},    
-{ id:3, name:"Ryzen_5_7500f", price:115, quantity:1,},    
-];
+cart = [];
+
 console.log(cart);
+
+function save_cart() {
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+};
+
+function load_cart() {
+    const cart = JSON.parse(sessionStorage.getItem("cart"));
+};
+
