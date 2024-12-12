fc0bbc43c920c9d8e79ba5e91d3aa32c0d1b48c7
derphilovic <philovic@outlook.de>
2024-12-12 20:31:27 +0100

    -m

---
+++ script.js
@@ -0,0 +1,6 @@
+const cart = [
+{ id:1, name:"Ryzen_5_7500f", price:115, quantity:1,},    
+{ id:2, name:"Ryzen_5_7500f", price:115, quantity:1,},    
+{ id:3, name:"Ryzen_5_7500f", price:115, quantity:1,},    
+];
+console.log(cart);
