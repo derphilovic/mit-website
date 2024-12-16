b48039875034d9cfd055ccdddfff8ef8c1c69186
derphilovic <philovic@outlook.de>
2024-12-16 21:28:50 +0100

    cart update

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const cart = [
     {id: 3, name:"r5_7500f", price:115, quantity:0,},
     {id: 4, name:"r5_7500f", price:115, quantity:0,},
     {id: 5, name:"r5_7500f", price:115, quantity:0,},
+    {id: 6, name:"r5_7500f", price:115, quantity:0,},
+    {id: 7, name:"r5_7500f", price:115, quantity:0,},
+    {id: 8, name:"r5_7500f", price:115, quantity:0,},
 ];
 
 console.log(cart);
@@ -15,11 +18,14 @@ function save_cart() {
 function load_cart() {
     const cart = JSON.parse(sessionStorage.getItem("cart"));
 };
+function add_item(x) {
 
-var button1 = document.getElementById("1");
-
-if (button1) {
-  objIndex = cart.findIndex(obj => obj.id = 1);
+  if (x === undefined) { x = 1;}
+  button1 = true; 
+  objIndex = cart.findIndex(obj => obj.id === x);
   cart[objIndex].quantity = cart[objIndex].quantity + 1;
   console.log(cart);
-}
+  window.alert(cart[objIndex].quantity);
+  save_cart();
+}; 
+
