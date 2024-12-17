5b59b7d68586d57ff2be1d01b3907d63a77ef0ce
derphilovic <philovic@outlook.de>
2024-12-17 20:51:19 +0100

    price calculation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const cart = [
-    {id: 1, name:"r7_7800X3D", price:115, quantity:0,},
-    {id: 2, name:"r9_9900X", price:115, quantity:0,},
-    {id: 3, name:"r7_9800X3D", price:115, quantity:0,},
-    {id: 4, name:"r5_7500f", price:115, quantity:0,},
-    {id: 5, name:"rx_7900XTX", price:115, quantity:0,},
-    {id: 6, name:"Herr Dreier", price:115, quantity:0,},
-    {id: 7, name:"rx_7700XT", price:115, quantity:0,},
-    {id: 8, name:"r7_7800X3D", price:115, quantity:0,},
+    {id: 1, name:"r7_7800X3D", price:455, quantity:0, finalprice: 0,},
+    {id: 2, name:"r9_9900X", price:470, quantity:0, finalprice: 0,},
+    {id: 3, name:"r7_9800X3D", price:760, quantity:0, finalprice: 0,},
+    {id: 4, name:"r5_7500f", price:122, quantity:0, finalprice: 0,},
+    {id: 5, name:"rx_7900XTX", price:910, quantity:0, finalprice: 0,},
+    {id: 6, name:"Herr Dreier", price:69420, quantity:0, finalprice: 0,},
+    {id: 7, name:"rx_7700XT", price:540, quantity:0, finalprice: 0,},
+    {id: 8, name:"r7_7800X3D", price:455, quantity:0, finalprice: 0,},
 ];
 
 console.log(cart);
@@ -34,15 +34,27 @@ function get_quantity(y)
 quantity = cart[y].quantity;
 };
 
+function get_price(y)
+{
+price = cart[y].price;
+};
+
+function price_calc(y){
+    get_price(y);
+    get_quantity(y);
+    cart[y].finalprice = (quantity * price);
+    finalprice = (quantity * price);
+    }
+
 function alert_cart() {
     i = 0
 while ( i < (cart.length + 1)) {
+    price_calc(i);
     get_quantity(y = i);
     if (quantity > 0)
     {
-window.alert(cart[i].quantity + ", " + cart[i].name);
+window.alert(cart[i].quantity + ", " + cart[i].name + " Price for that is: " + finalprice + "€");
 };
 i++;
 };
 };
-
