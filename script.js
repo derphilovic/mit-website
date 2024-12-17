0bd2a43d11a1b5b3e3d4137648878e2096c3a402
derphilovic <philovic@outlook.de>
2024-12-17 17:56:02 +0100

    cart display content

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const cart = [
-    {id: 1, name:"r5_7500f", price:115, quantity:0,},
-    {id: 2, name:"r5_7500f", price:115, quantity:0,},
-    {id: 3, name:"r5_7500f", price:115, quantity:0,},
+    {id: 1, name:"r7_7800X3D", price:115, quantity:0,},
+    {id: 2, name:"r9_9900X", price:115, quantity:0,},
+    {id: 3, name:"r7_9800X3D", price:115, quantity:0,},
     {id: 4, name:"r5_7500f", price:115, quantity:0,},
-    {id: 5, name:"r5_7500f", price:115, quantity:0,},
-    {id: 6, name:"r5_7500f", price:115, quantity:0,},
-    {id: 7, name:"r5_7500f", price:115, quantity:0,},
-    {id: 8, name:"r5_7500f", price:115, quantity:0,},
+    {id: 5, name:"rx_7900XTX", price:115, quantity:0,},
+    {id: 6, name:"Herr Dreier", price:115, quantity:0,},
+    {id: 7, name:"rx_7700XT", price:115, quantity:0,},
+    {id: 8, name:"r7_7800X3D", price:115, quantity:0,},
 ];
 
 console.log(cart);
 
 function save_cart() {
-    sessionStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("cart", JSON.stringify(cart));
 };
 
 function load_cart() {
-    const cart = JSON.parse(sessionStorage.getItem("cart"));
+    cart = JSON.parse(localStorage.getItem("cart"));
 };
 function add_item(x) {
 
@@ -27,5 +27,23 @@ function add_item(x) {
   console.log(cart);
   window.alert(cart[objIndex].quantity);
   save_cart();
-}; 
+};
+
+function get_quantity(y)
+{
+quantity = cart[y].quantity;
+};
+
+function alert_cart() {
+    i = 0
+while ( i < (cart.length + 1)) {
+    get_quantity(y = i);
+    if (quantity > 0)
+    {
+window.alert(cart[i].quantity + ", " + cart[i].name);
+};
+i++;
+};
+display_list();
+};
 
