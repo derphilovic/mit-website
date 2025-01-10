7ad20d114a5e2c311ff93e4197c0934634cbb017
derphilovic <philovic@outlook.de>
2025-01-10 13:26:44 +0100

    cart almost only css needed

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let cart = [
     {id: 7, name:"rx_7700XT", price:540, quantity:0, finalprice: 0, img:"",},
     {id: 8, name:"r7_7800X3D", price:455, quantity:0, finalprice: 0, img:"",},
 ];
-
 console.log(cart);
 
 function save_cart() {
@@ -16,7 +15,7 @@ function save_cart() {
 };
 
 function load_cart() {
-    const savedCart = JSON.parse(localStorage.getItem("cart"));
+    savedCart = JSON.parse(localStorage.getItem("cart"));
     if (savedCart) {
     cart.length = 0;
     savedCart.forEach(item => cart.push(item));
@@ -31,7 +30,7 @@ function add_item(x) {
     cart[objIndex].quantity = cart[objIndex].quantity + 1;
   };
   console.log(cart);
-  window.alert(cart[objIndex].quantity);
+  window.alert("Product added!");
   save_cart();
 };
 
@@ -66,37 +65,76 @@ i++;
 };
 
 function addCartItem(img, Names, price, id) {
-    const cartList = document.querySelector("#cart ul"); 
-    const li = document.createElement("li");
+    cartList = document.querySelector("#cart ul"); 
+    li = document.createElement("li");
     li.innerHTML = `
-    <div class="item">
+    <div class="item" id="${price}">
      <img src="${img}" alt="">
     <p id="1me">${Names}</p>
     <label for="Quantity">Quantity</label>
-    <select name="Quantity" id="${id}">
+    <select name="Quantity" id="${id}" onchange="update_quantity(this.value, ${id})">
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
     <option value="4">4</option>
     </select>
     <p>${price}</p>
-    <button><i class="fa-solid fa-xmark"></i></button>
+    <button type="button" onclick="remove_item();"><i class="fa-solid fa-xmark"></i></button>
     </div>
 `;
- cartList.appendChild(li);
-}
+    cartList.appendChild(li);
+};
+
+function update_quantity(value, id) {
+    objIndex = cart.findIndex(obj => obj.id === id);
+    cart[objIndex].quantity = parseInt(value);
+    price_calc(objIndex);
+    selectElement = document.getElementById(id);
+    priceElement = selectElement.parentElement.querySelector('p:last-of-type');
+    priceElement.textContent = cart[objIndex].finalprice + '€';
+    save_cart();
+    total_price();
+};
+
+function remove_item() {
+    x = event.target.closest("button");
+    if(x) {
+    item = x.closest(".item");
+    id = parseInt(item.querySelector("select").id);
+    objIndex = cart.findIndex(obj => obj.id === id);
+    cart[objIndex].quantity = 0;
+    cart[objIndex].finalprice = 0;
+    item.closest("li").remove();
+    save_cart();
+    total_price();
+};
+};
+
+function total_price() {
+    let x = 0;
+    let pr = 0;
+    let tar = document.getElementById("Button3");
+    tar.textContent = "";
+    while(x < cart.length) {
+    pr += cart[x].finalprice;
+    x++;
+    console.log(pr);
+};
+    let txt = document.createTextNode("Gesamtpreis: " + pr + "€");
+    tar.appendChild(txt);
+};
 
 function cart_loader() {
     load_cart();
     x = 0;
-    while(x <= cart.length) {
-        price_calc(x);
+    while(x < cart.length) {
+    price_calc(x);
     if(cart[x].quantity >=1) {
-        addCartItem(cart[x].img, cart[x].name, cart[x].finalprice+"€", cart[x].id)
+    addCartItem(cart[x].img, cart[x].name, cart[x].finalprice+"€", cart[x].id)
 };
 x++;
 };
+total_price();
 };
-
 
 cart_loader();
