eab008726c9ba2422b1fca85ee757d972c9a4cb7
derphilovic <philovic@outlook.de>
2025-01-16 21:31:41 +0100

    s

--- README.md
+++ README.md
@@ -6,3 +6,21 @@ Der ordner "message_db" muss in die Directory
 ansonsten wird Login und das Kontaktformular nicht funktionierten
 
 Die SQL-Database kann man bei "localhost/phpmyadmin" im browser öffnen
+Falls dies nicht Funktioniert erstellen sie in phpMyAdmin eine
+Database namens "message_db"
+Diese Db braucht eine Tabelle Namens "message" mit 5 spalten
+Name        Typ       länge     A_I
+1. id       int                 true
+2. name     varchar   128       false
+3. message  varchar   128       false
+4. email    varchar   128       false
+5. typofmsg int                 false
+
+Danach müssen sie eine Db namens "users" mit 6 spalten machen
+Name          Typ       länge     A_I
+1. id         int                 true
+2. name       varchar   128       false
+3. username   varchar   128       false
+4. email      varchar   128       false
+5. adress     varchar   128       false
+6. password   varchar   128       false
--- account-login.php
+++ account-login.php
@@ -23,5 +23,39 @@ if (mysqli_connect_errno()) {
  };
 
  $sql = "SELECT id, username, email, password FROM users";
+ $result = $conn->query($sql);
 
-  
+ if ($result->num_rows > 0) {
+    $valid = 0;
+    $validpswrd = 0;
+     while($row = $result->fetch_assoc()) {
+       if ($row["email"] == $email) {
+           $valid = 1;
+       }
+       if ($row["username"] == $usrname){
+        $valid = 1;
+       }
+       if ($row["password"] == $pswrd){
+        $validpswrd = 1;
+       }
+     }
+     if (! $valid) { 
+        die("Kein Benutzername oder Email gefunden");
+      }
+    if (! $validpswrd) {
+        die("Falsches Passwort!");
+    }
+   } 
+
+echo"Eingeloggt!";
+
+/*
+dies ist das ende des Projektes...
+Ich hab grad keinen Bock weiter zu Programmieren...
+Vielleicht werd ich mich in Zukunft nochmal dransetzen
+Im laufe des Projektes hab ich einiges neues gelernt
+w.z.B. SQL und PHP, dazu hab ich einiges neues über Javascript gelernt.
+Ich fand das Projekt echt cool und ich hoffe das wir bald wieder sowas machen.
+falls sie ein secret möchten gucken sie das 
+https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUXbmV2ZXIgZ29ubmEgZ2l2ZSB5b3UgdXA%3D
+*/
